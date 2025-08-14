0f5971c63ed50e5bf6796ff3939821424ec7dcca
Austyn Chen <87254313+achen0206@users.noreply.github.com>
2025-08-13 21:34:30 -0700

    Update LateralController.py

--- competition_code/LateralController.py
+++ competition_code/LateralController.py
@@ -12,5 +12,15 @@ class Lateral:
     local_x = dx*math.cos(-vehicle_orientation) - dy*math.sin(-vehicle_orientation)
     local_y = dy*math.sin(-vehicle_orientation) + dx*math.cos(-vehicle_orientation)
 
+   if ld == 0:
+        return 0.0
+
+    
+    alpha = math.atan2(local_y, local_x)
+
+    steering = math.atan2(2 *4.7 * math.sin(alpha), ld)
+
+    return steering
+
     
   #Find algo to calculate steering_angle (Pure Pursuit)
