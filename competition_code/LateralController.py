d348e2a626990f9de1d12412e840e874fd84f9db
Austyn Chen <87254313+achen0206@users.noreply.github.com>
2025-08-22 23:52:02 -0700

    Update LateralController.py

--- competition_code/LateralController.py
+++ competition_code/LateralController.py
@@ -6,8 +6,8 @@ class LatController:
     #calculates steering angle for the car
   
     #Find distance between vehicle and waypoint
-    dx = waypoint[0] - current_location[0]
-    dy = waypoint[1] - current_location[1]
+    dx = waypoint.location[0] - current_location[0]
+    dy = waypoint.location[1] - current_location[1]
   
     #Convert to local coordinates
     local_x = dx*math.cos(-vehicle_orientation[2]) - dy*math.sin(-vehicle_orientation[2])
